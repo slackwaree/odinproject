70b80d421b739d2fec6a719db7de7ac4fa6662d9
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-07-23 13:32:10 +1200

    Add main.js.

--- paper-scissors-rock/main.js
+++ paper-scissors-rock/main.js
@@ -1,58 +1,68 @@
 function playRound() {
   function playGame() {
-    let getInput = prompt("Type 'rock', 'paper', or 'scissors'");
+    let getHumanChoice = prompt("Type 'rock', 'paper', or 'scissors'");
+    getHumanChoice = getHumanChoice.toLowerCase();
 
-    if (getInput != "paper" && "scissors" && "rock") {
-        console.log("fail");
-        return;
+    if (getHumanChoice !== "paper" && getHumanChoice !== "scissors" && getHumanChoice !== "rock") { // if input invalid
+        roundNumber--;
+        return; // Adds another game if the player input is invalid
     }
-
     let computerResponses = ["rock", "paper", "scissors"];
-    let randomResult = computerResponses[Math.floor(Math.random()*computerResponses.length)];
+    let getComputerChoice = computerResponses[Math.floor(Math.random()*computerResponses.length)];
     
     switch (true) { // holy fuck thats a lot of switch statements :sob:
-      case getInput == "rock" && randomResult == "rock": // rock on rock
+      case getHumanChoice == "rock" && getComputerChoice == "rock": // rock on rock
         console.log("User chose ROCK. Computer chose ROCK.\n The result is a Draw.");
         return;
-      case getInput == "rock" && randomResult == "paper": // rock on paper
+      case getHumanChoice == "rock" && getComputerChoice == "paper": // rock on paper
         console.log("User chose ROCK. Computer chose PAPER.\n Computer Wins.");
         computerScore++;
         return;
-      case getInput == "rock" && randomResult == "scissors": // rock on scissors
+      case getHumanChoice == "rock" && getComputerChoice == "scissors": // rock on scissors
         console.log("User chose ROCK. Computer chose SCISSORS.\n User wins.");
         humanScore++;
         return;
-      case getInput == "paper" && randomResult == "rock": // paper on rock
+      case getHumanChoice == "paper" && getComputerChoice == "rock": // paper on rock
         console.log("User chose PAPER. Computer chose ROCK.\n User wins.");
         humanScore++;
         return;
-      case getInput == "paper" && randomResult == "paper": // paper on paper
+      case getHumanChoice == "paper" && getComputerChoice == "paper": // paper on paper
         console.log("User chose PAPER. Computer chose PAPER.\n The result is a draw.");
         return;
-      case getInput == "paper" && randomResult == "scissors": // paper on scissors
+      case getHumanChoice == "paper" && getComputerChoice == "scissors": // paper on scissors
         console.log("User chose PAPER. Computer chose SCISSORS.\n Computer wins.");
         computerScore++;
         return;
-      case getInput == "scissors" && randomResult == "rock": // scissors on rock
+      case getHumanChoice == "scissors" && getComputerChoice == "rock": // scissors on rock
         console.log("User chose SCISSORS. Computer chose ROCK.\n Computer wins.");
         computerScore++;
         return;
-      case getInput == "scissors" && randomResult == "paper": // scissors on paper
+      case getHumanChoice == "scissors" && getComputerChoice == "paper": // scissors on paper
         console.log("User chose SCISSORS. Computer chose PAPER.\n User wins.");
         humanScore++;
         return;
-      case getInput == "scissors" && randomResult == "scissors": // scissors on scissors (lesbian refernc)
+      case getHumanChoice == "scissors" && getComputerChoice == "scissors": // scissors on scissors (lesbian refernc)
         console.log("User chose SCISSORS. Computer chose SCISSORS.\n The result is a draw.");
         return;
     }
   }
     let computerScore = 0;
     let humanScore = 0;
-    let roundNumber = 1;
+    let roundNumber = 0;
     
-    for (let i = 0; i < 5; i++) { // game loop.
+    while (roundNumber != 5) { // game loop
       playGame(computerScore, humanScore);
       console.log("Computer score: " + computerScore + "\n Human score: " + humanScore);
+      roundNumber++;
+    }
+    if (computerScore > humanScore) {
+        console.log("Game over. Computer wins!");
+    }
+    else if (computerScore < humanScore) {
+        console.log("Game over. Human wins!");
+    }
+    else {
+        console.log("Game over. It's a tie!");
     }
 }
 playRound();
