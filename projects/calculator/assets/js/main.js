9903d71977ed2c538b53e590ee880ed7892604e1
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-09-01 15:40:44 +1200

    Add projects/calculator

---
+++ projects/calculator/assets/js/main.js
@@ -0,0 +1,76 @@
+/* 
+I created my previous calculator with the help of a YouTube tutorial in which eval() was used.
+I later learned that using eval() is dangerous in a real world context so I have opted for
+another method. 95% of this was made with my own practical knowledge.
+*/
+
+const operatorOrNumberBtn = document.querySelectorAll('.number, .operator');
+const display = document.querySelector('.display');
+
+let firstNum = '';
+let secondNum = '';
+let operatorSelection;
+const OPERATORS = ['+', '-', '*', '/'];
+
+operatorOrNumberBtn.forEach(element => {
+    element.addEventListener('click', () => {
+        switch (true) {
+            case element.value == 'clear':
+                display.textContent = '';
+                operatorSelection = null;
+                firstNum = '';
+                secondNum = '';
+                return;
+            case element.value == '=':
+                if (operatorSelection != null && firstNum != '' && secondNum != '') {
+                    operate();
+                    return;
+                }
+            case OPERATORS.indexOf(element.value) != -1 && operatorSelection == null: // check for operator and if an operator has been selected and is NaN
+                operatorSelection = element.value;
+                display.textContent += element.textContent;
+                return;
+            case operatorSelection == null: // if operatorSelection is null, append value of display.value to firstNum
+                firstNum += element.value;
+                display.textContent += element.textContent;
+                return;
+            case operatorSelection != null && firstNum != '': // if operatorSelection is NOT null, append value of display.value to secondNum
+                secondNum += element.value;
+                display.textContent += element.textContent;
+                return;
+        }
+    });
+});
+
+function operate() {
+    firstNum = this.parseFloat(firstNum);
+    secondNum = this.parseFloat(secondNum);
+    
+    switch (true) {
+        case firstNum == 0 && operatorSelection == '/' && secondNum == 0:
+            display.textContent = 'brah.';
+            break;
+        case operatorSelection == '+':
+            display.textContent = firstNum + secondNum;
+            break;
+        case operatorSelection == '-':
+            display.textContent = firstNum - secondNum;
+            break;
+        case operatorSelection == '*':
+            display.textContent = firstNum * secondNum;
+            break;
+        case operatorSelection == '/':
+            display.textContent = firstNum / secondNum;
+            break;
+    }
+    firstNum = display.textContent; // The next calculation will use the result as firstNum
+    secondNum = '';
+    operatorSelection = null;
+}
+
+/* TO DO
+Add backspace button
+Add keyboard support
+Empty display on number input
+Add disabled property to button on decimal input
+*/
--- rockpaperscissors/assets/scripts/main.js
+++ rockpaperscissors/assets/scripts/main.js
@@ -1,68 +1,9 @@
-let wrapper = document.getElementById('flex-container');
+let choiceBtn = document.querySelector("choice-btn");
+let playerChoice;
 
-let getHumanChoice;
-let computerChoice = ["rock", "paper", "scissors"];
+let btnClickSfx = new Audio('../assets/audio/click.mp3');
 
-let humanScore = 0;
-let computerScore = 0;
-
-const computerScoreHTML = document.getElementById("computer-score");
-const humanScoreHTML = document.getElementById("human-score");
-
-wrapper.addEventListener('click', (e) => {
-    const isButton = e.target.nodeName === 'BUTTON';
-    if (!isButton) {
-        return;
-    }
-
-    getHumanChoice = e.target.id;
-    getComputerChoice = computerChoice[Math.floor(Math.random()*computerChoice.length)];
-
-    const results = document.getElementById("results").classList.remove("hidden");
-    document.getElementById("player-choice").textContent = getHumanChoice;
-    document.getElementById("computer-choice").textContent = getComputerChoice;
-
-    switch (true) { // holy fuck thats a lot of switch statements :sob:
-        case getHumanChoice == "rock" && getComputerChoice == "rock": // rock on rock
-            break;
-        case getHumanChoice == "rock" && getComputerChoice == "paper": // rock on paper
-            computerScore++;
-            computerScoreHTML.textContent = computerScore;
-            break;
-        case getHumanChoice == "rock" && getComputerChoice == "scissors": // rock on scissors
-            humanScore++;
-            humanScoreHTML.textContent = humanScore;
-            break;
-        case getHumanChoice == "paper" && getComputerChoice == "rock": // paper on rock
-            humanScore++;
-            humanScoreHTML.textContent = humanScore;
-            break;
-        case getHumanChoice == "paper" && getComputerChoice == "paper": // paper on paper
-            break;
-        case getHumanChoice == "paper" && getComputerChoice == "scissors": // paper on scissors
-            computerScore++;
-            computerScoreHTML.textContent = computerScore;
-            break;
-        case getHumanChoice == "scissors" && getComputerChoice == "rock": // scissors on rock
-            computerScore++;
-            computerScoreHTML.textContent = computerScore;
-            break;
-        case getHumanChoice == "scissors" && getComputerChoice == "paper": // scissors on paper
-            humanScore++;
-            humanScoreHTML.textContent = humanScore;
-            break;
-        case getHumanChoice == "scissors" && getComputerChoice == "scissors": // scissors on scissors (lesbian refernc)
-            break;
-    }
-    if (humanScore == 5 || computerScore == 5) {
-        wrapper.classList.add("hidden");
-        roundResults = document.getElementById("round-results");
-
-        if (computerScore > humanScore) {
-            roundResults.textContent = "Computer wins.";
-        }
-        else {
-            roundResults.textContent = "Player wins.";
-        }
-    }
-})
+choiceBtn.addEventListener("click", function() {
+    btnClickSfx.play();
+    choiceBtn.classList.add("invis");
+});
