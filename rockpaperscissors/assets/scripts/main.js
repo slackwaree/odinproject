bd2cd45a539194caedf0ecaed05b1afe60d5978d
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-09-01 15:56:28 +1200

    Change file structure

--- rockpaperscissors/assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-let choiceBtn = document.querySelector("choice-btn");
-let playerChoice;
-
-let btnClickSfx = new Audio('../assets/audio/click.mp3');
-
-choiceBtn.addEventListener("click", function() {
-    btnClickSfx.play();
-    choiceBtn.classList.add("invis");
-});
