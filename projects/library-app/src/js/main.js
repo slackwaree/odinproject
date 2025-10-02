9cd917db320b676336804d655efdcf3ec6eb02be
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-10-03 10:19:28 +1300

    fr*cking awesome commit message

--- projects/library-app/src/js/main.js
+++ projects/library-app/src/js/main.js
@@ -5,44 +5,48 @@ const docFrag = document.createDocumentFragment();
 
 const submitBtn = document.querySelector('.add-book-submit');
 submitBtn.addEventListener('click', () => { // Function to create book entry
-    let newBookEntry = document.createElement('div');
-    newBookEntry.classList.add('grid-item');
-    container.appendChild(newBookEntry);
-    
-    const bookContents = document.createElement('div');
-    bookContents.classList.add('book-contents');
-
-    newBookEntry.appendChild(bookContents);
-    bookContents.appendChild(docFrag);
-    
     const getBookTitle = document.getElementById('title').value;
     const getBookAuthor = document.getElementById('author').value;
     const getBookPageCount = document.getElementById('pages').value;
     const getBookIsRead = document.getElementById('isRead').checked;
 
-    const book = new Book(getBookTitle, getBookAuthor, getBookPageCount, getBookIsRead);
+    if (getBookTitle != '' && getBookAuthor != '' && getBookPageCount != '') {
+        let newBookEntry = document.createElement('div');
+        newBookEntry.classList.add('grid-item');
+        container.appendChild(newBookEntry);
+        
+        const bookContents = document.createElement('div');
+        bookContents.classList.add('book-contents');
     
-    for (let key in book) {
-        let h3Create = document.createElement('h3');
-        h3Create.textContent = `${book[key]}`;
-        bookContents.appendChild(h3Create);
-    }
-    const createBtnContainer = document.createElement('div');
-    createBtnContainer.classList.add('btn-container');
-    bookContents.appendChild(createBtnContainer);
+        newBookEntry.appendChild(bookContents);
+        bookContents.appendChild(docFrag);
 
-    const createBtn = document.createElement('button');
-    createBtn.classList.add('isRead-btn');
-    createBtnContainer.appendChild(createBtn);
-
-    formContainer.classList.toggle('hidden');
-
-    let isReadBtn = document.querySelectorAll('.isRead-btn'); // update node list;
-    isReadBtn.forEach(item => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('is-read');
+        const book = new Book(getBookTitle, getBookAuthor, getBookPageCount, getBookIsRead);
+        for (let key in book) {
+            let h3Create = document.createElement('h3');
+            h3Create.textContent = `${book[key]}`;
+            bookContents.appendChild(h3Create);
+        }
+        const createBtnContainer = document.createElement('div');
+        createBtnContainer.classList.add('btn-container');
+        bookContents.appendChild(createBtnContainer);
+    
+        const createBtn = document.createElement('button');
+        createBtn.classList.add('isRead-btn');
+        createBtnContainer.appendChild(createBtn);
+    
+        if (getBookIsRead) { // if getBookIsRead checked
+            createBtn.classList.add('is-read');
+        }
+        formContainer.classList.toggle('hidden');
+    
+        let isReadBtn = document.querySelectorAll('.isRead-btn'); // update node list;
+        isReadBtn.forEach(item => {
+            item.addEventListener('click', () => {
+                item.classList.toggle('is-read');
+            })
         })
-    })
+    }
 });
 
 const newBookBtn = document.querySelector('.new-book-btn');
@@ -50,7 +54,7 @@ newBookBtn.addEventListener('click', () => {
     formContainer.classList.toggle('hidden');
 });
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages) {
     this.title = 'Title: ' + title,
     this.author = 'Author: ' + author,
     this.pages = pages + ' pages';
