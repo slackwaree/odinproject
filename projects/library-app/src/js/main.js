9e0665d06886094b3ea72660d3cf6cb54c5a0c13
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-10-06 11:39:58 +1300

    Fix bug where multiple event listeners are addded to removeBookBtn and createBtn

--- projects/library-app/src/js/main.js
+++ projects/library-app/src/js/main.js
@@ -3,13 +3,34 @@ const formContainer = document.querySelector('.form-container');
 const submitBtn = document.querySelector('.add-book-submit');
 const docFrag = document.createDocumentFragment();
 
+const newBookBtn = document.querySelector('.new-book-btn');
+newBookBtn.addEventListener('click', () => {
+    formContainer.classList.toggle('hidden');
+});
+
+const myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = 'Title: ' + title,
+    this.author = 'Author: ' + author,
+    this.pages = pages + ' pages';
+}
+function addBookToLibrary(getBookTitle, getBookAuthor, getBookPageCount) {
+    const newBook = {
+        title: getBookTitle,
+        author: getBookAuthor,
+        pages: getBookPageCount,
+        crypto: crypto.randomUUID(),
+    }
+    myLibrary.push(newBook);
+}
 submitBtn.addEventListener('click', () => { // Function to create book entry
     const getBookTitle = document.getElementById('title').value;
     const getBookAuthor = document.getElementById('author').value;
     const getBookPageCount = document.getElementById('pages').value;
     const getBookIsRead = document.getElementById('isRead').checked;
 
-    if (getBookTitle != '' && getBookAuthor != '' && getBookPageCount != '') { // failsafe
+    if (getBookTitle != '' && getBookAuthor != '' && getBookPageCount != '') { // form validation
         let newBookEntry = document.createElement('div');
         newBookEntry.classList.add('grid-item');
         container.appendChild(newBookEntry);
@@ -22,52 +43,33 @@ submitBtn.addEventListener('click', () => { // Function to create book entry
 
         const removeBookBtn = document.createElement('div');
         removeBookBtn.classList.add('remove-book-btn');
+        removeBookBtn.addEventListener('click', () => {
+            removeBookBtn.parentElement.parentElement.remove();
+        })
         bookContents.appendChild(removeBookBtn);
 
-        const book = new Book(getBookTitle, getBookAuthor, getBookPageCount, getBookIsRead);
+        const book = new Book(getBookTitle, getBookAuthor, getBookPageCount);
         for (let key in book) {
             let h3Create = document.createElement('h3');
             h3Create.textContent = `${book[key]}`;
             bookContents.appendChild(h3Create);
-        }
+        }        
         const createBtnContainer = document.createElement('div');
         createBtnContainer.classList.add('btn-container');
         bookContents.appendChild(createBtnContainer);
     
         const createBtn = document.createElement('button');
-        createBtn.classList.add('isRead-btn');
-        createBtnContainer.appendChild(createBtn);
-    
-        if (getBookIsRead) { // if getBookIsRead checked
+        if (getBookIsRead) { // if getBookIsRead.checked()
             createBtn.classList.add('is-read');
         }
+        createBtn.addEventListener('click', () => {
+            createBtn.classList.toggle('is-read');
+        })
+        createBtnContainer.appendChild(createBtn);
         formContainer.classList.toggle('hidden');
-    
-        let isReadBtn = document.querySelectorAll('.isRead-btn'); // update node list;
-        isReadBtn.forEach(item => {
-            item.addEventListener('click', () => {
-                item.classList.toggle('is-read');
-            });
-        });
+        addBookToLibrary(getBookTitle, getBookAuthor, getBookPageCount);
     }
     else {
-        window.alert('Please fill out all fields.')
+        window.alert('Please fill out all fields.');
     }
-    let removeBookBtnList = document.querySelectorAll('.remove-book-btn'); // function to remove book entries
-    removeBookBtnList.forEach(item => {
-        item.addEventListener('click', () => {
-            item.parentElement.parentElement.remove();
-        });
-    });
 });
-
-const newBookBtn = document.querySelector('.new-book-btn');
-newBookBtn.addEventListener('click', () => {
-    formContainer.classList.toggle('hidden');
-});
-
-function Book(title, author, pages) {
-    this.title = 'Title: ' + title,
-    this.author = 'Author: ' + author,
-    this.pages = pages + ' pages';
-}
